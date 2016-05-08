6113ef7ce1f5b4e6b7fbfbd97b3bf0850713e866
王乐天 <koh.718@gmail.com>
2016-05-08 21:14:08 +0800

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup /root/jdk1.8.0_91/bin/java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active="prod" -Denv="prod" -jar build/libs/evpn-service-1.0.0.jar > /dev/null 2>&1 &
+nohup /root/jdk1.8.0_92/bin/java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active="prod" -Denv="prod" -jar build/libs/evpn-service-1.0.0.jar > /dev/null 2>&1 &
